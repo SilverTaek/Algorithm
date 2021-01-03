e9ea0b2909641705fca051f199ed55e6874c2edf
rudus1012 <rudus1012@naver.com>
2021-01-03 20:13:22 +0900

    프로그래머스 문제풀이

---
+++ Programmers/Lev2/lev2_문자열압축.java
@@ -0,0 +1,15 @@
+package solution;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        for (int i = 0; i < s.length()/2; i++) {
+			if(s.charAt(i)==s.charAt(i+1)) {
+				
+			}
+		}
+        
+        
+        return answer;
+    }
+}
