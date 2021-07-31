955ed96eea62ecc12552f74934484b330815804d
rudus1012 <rudus1012@naver.com>
2021-07-31 23:11:02 +0900

    문제 풀이

---
+++ Codility/Co_2_2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] A) {
+        int result = 0;
+
+        for(int i=0; i<A.length; i++){
+            result = result ^ A[i];
+        }
+
+        return result;
+    }
+}
