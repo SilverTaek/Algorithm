1b85e186467e63009e8f7a2c478f552b1bcb1cb3
rudus1012 <rudus1012@naver.com>
2021-08-23 21:03:21 +0900

    문제풀이

--- Boj/Boj_2667.java
+++ Boj/Boj_2667.java
@@ -1,87 +1,63 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
- 
+
 public class Boj_2667 {
- 
-    static int N;
-    static int[][] map;
-    static boolean[][] visited;
-    static Queue<Node> q = new LinkedList<>();
-    static ArrayList<Integer> list = new ArrayList<>();
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
+	
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
+    static int N, group_cnt;
+    static String[] a;
+    static boolean[][] visit;
+    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static ArrayList<Integer> group;
+
+    static void input() throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        
-        map = new int[N][N];
-        visited = new boolean[N][N];
-        
-        for (int i = 0; i < N; i++) {
-            char[] temp = br.readLine().toCharArray();
-            for (int j = 0; j < N; j++) {
-                map[i][j] = temp[j] - '0';
-            }
+    	
+        a = new String[N];
+        for (int i = 0; i < N; i++)
+            a[i] = br.readLine();
+        visit = new boolean[N][N];
+    }
+
+    // x, y 를 갈 수 있다는 걸 알고 방문한 상태
+    static void dfs(int x, int y) {
+        group_cnt++;
+        visit[x][y] = true;
+        for (int k = 0; k < 4; k++) {
+            int nx = x + dir[k][0];
+            int ny = y + dir[k][1];
+            if (nx < 0 || ny < 0 || nx >= N || ny >= N) continue;  // 지도를 벗어나는 곳으로 가는가?
+            if (a[nx].charAt(ny) == '0') continue;  // 갈 수 있는 칸인지 확인해야 한다.
+            if (visit[nx][ny]) continue;  // 이미 방문한 적이 있는 곳인가?
+            dfs(nx, ny);
         }
-        
-        int mark = 0;
-        
+    }
+
+    static void pro() {
+        group = new ArrayList<>();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(map[i][j] == 0 || visited[i][j])
-                    continue;
-                int cnt = 0;
-                visited[i][j] = true;
-                q.add(new Node(i, j));
-                
-                while(!q.isEmpty()) {
-                    
-                    Node cur = q.poll();
-                    cnt++;
-                    
-                    for (int k = 0; k < 4; k++) {
-                        int nx = cur.x + dx[k];
-                        int ny = cur.y + dy[k];
-                        
-                        if(!isRange(nx, ny) || visited[nx][ny] || map[nx][ny] == 0)
-                            continue;
-                        
-                        visited[nx][ny] = true;
-                        q.add(new Node(nx, ny));
-                    }
+                if (!visit[i][j] && a[i].charAt(j) == '1') {
+                    // 갈 수 있는 칸인데, 이미 방문처리 된, 즉 새롭게 만난 단지인 경우!
+                    group_cnt = 0;
+                    dfs(i, j);
+                    group.add(group_cnt);
                 }
-                list.add(cnt);
-                mark++;
             }
         }
-        
-        Collections.sort(list);
-        System.out.println(mark);
-        for(int cnt : list) {
-            System.out.println(cnt);
-        }
+
+        Collections.sort(group);
+        System.out.println(group.size());
+        for (int cnt: group) System.out.println(cnt);
+     
     }
-    
-    static boolean isRange(int x, int y) {
-        if( x < 0 || x >= N || y < 0 || y >= N) return false;
-        return true;
-    }
-}
- 
-class Node {
-    int x;
-    int y;
-    
-    Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        pro();
     }
 }
